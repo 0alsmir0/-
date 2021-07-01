411e2371ebc426b4c252504e1044591a39ea53fb
0alsmir0 <86766857+0alsmir0@users.noreply.github.com>
2021-07-01 10:16:46 +0300

    Add files via upload

---
+++ ideone_w192tx.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+double F(double x){
+return x*x-9*x+14;}
+double Root(double (*f)(double),double a,double b,double eps){
+double c;
+while((b-a)/2>eps){
+ c=(a+b)/2;
+ if((f(a)*f(c))>0) a=c;
+ else b=c;
+}
+return c;}
+int main(){
+double a,b,eps,x;
+cout << "interval: ";
+cin >> a;
+cin >> b;
+if(F(a)*F(b)>0){
+ cout << "Wrong interval!\n";
+ return 0;}
+cout << "eror: ";
+cin >> eps;
+x=Root(F,a,b,eps);
+cout << "x = " << x << endl;
+return 0;
+}
